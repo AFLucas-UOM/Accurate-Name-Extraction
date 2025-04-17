374f3fca7b07fa295959db037cf1f216de8d84d9
AFLucas-UOM <andrea.f.lucas.22@um.edu.mt>
2025-04-17 19:00:43 +0200

    Survey part 1

--- 5. ANEP UI/src/App.tsx
+++ 5. ANEP UI/src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useTheme } from "./hooks/use-theme";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { SurveyVisualizerCore } from "./pages/SurveyVisualizerCore";
 
 const queryClient = new QueryClient();
 
@@ -25,6 +26,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/survey" element={<SurveyVisualizerCore />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
